eca88a1f121ede204394dad08a2176b3647b1b43
JOAO PEDRO CAVANHAS COLOMBO <joao.pccolombo@senacsp.edu.br>
2025-08-21 22:18:59 -0300

    feat: first commit in AP2

---
+++ src/BuscaLinear.java
@@ -0,0 +1,23 @@
+package src;
+
+public class BuscaLinear {
+
+    public static int buscar(int[] array, int valorProcurado) {
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == valorProcurado) {
+                System.out.println("\nBusca Linear: Valor: " + valorProcurado + " encontrado no índice " + i);
+                return i;
+            }
+        }
+        System.out.println("\nBuscaLinear: Valor: " + valorProcurado + " não encontrado");
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] idsCliente = {105, 201, 350, 422, 599, 608};
+        int idProcurado = 422;
+        int idInexistente = 999;
+
+        buscar(idsCliente, idProcurado);
+        buscar(idsCliente, idInexistente);
+    } 
+}
